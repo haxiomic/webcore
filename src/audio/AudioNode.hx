c426b6defe1ef7ba5538976de98412a51f8019e7
George Corney <haxiomic@gmail.com>
2020-01-09 21:58:23 +0000

    Simplify gain node and make more robust

--- src/audio/AudioNode.hx
+++ src/audio/AudioNode.hx
@@ -11,7 +11,6 @@ typedef AudioBufferSourceNode = js.html.audio.AudioBufferSourceNode;
 import cpp.*;
 import audio.native.AudioDecoder;
 import audio.native.NativeAudioSource;
-import audio.native.MiniAudio;
 
 @:allow(audio.AudioContext)
 class AudioNode {
@@ -144,31 +143,31 @@ private class PcmTransform {
     
     /**
         This is called from the unmanaged audio thread. It's critical that no haxe-allocation or haxe vm interaction occurs here
-        @! maybe move this to C
+        @! maybe move this and PcmTransformData to C
     **/
     @:noDebug static public function readFramesCallback<T>(source: Star<NativeAudioSource>, nChannels: UInt32, frameCount: UInt64, schedulingCurrentFrameBlock: UInt64, interleavedSamples: Star<Float32>): UInt64 {
         var readFramesData: Star<PcmTransformData<T>> = cast source.getUserData();
         var framesRead = AudioContext.mixSources(readFramesData.nativeSourceList, nChannels, frameCount, schedulingCurrentFrameBlock, interleavedSamples);
-        // apply user transform
-        // @! should only apply the transform function to frames that are actually read
-        readFramesData.transformFunction(readFramesData.transformData, nChannels, frameCount, cast interleavedSamples);
+        // apply user transform to frames read
+        readFramesData.transformFunction(readFramesData.transformDataStar, nChannels, framesRead, cast interleavedSamples);
         return framesRead;
     }
 
 }
 
+/**
+    `transformFunction` is executed on the audio thread, mutex locking must be used is the transform data is changed from the haxe thread
+    Additionally, it's critical no haxe-allocation or vm interaction occurs within `transformFunction`.
+**/
 @:generic private class PcmTransformNode<T> extends AudioNode {
 
     // we pass the address to these fields as function data (not their values)
     final readFramesData: PcmTransformData<T>;
-    final transformData: T; // used to ensure we have a reference for GC
 
     public function new(context: AudioContext, transformFunction: PcmTransformFunction<T>, transformData: T) {
         super(context);
-        this.transformData = transformData;
         
-        this.readFramesData = new PcmTransformData(Pointer.fromHandle(this.nativeSourceList), transformFunction);
-        this.readFramesData.transformData = cast Native.addressOf(this.transformData);
+        this.readFramesData = new PcmTransformData(Pointer.fromHandle(this.nativeSourceList), transformFunction, transformData);
 
         this.nativeSource.setUserData(cast Native.addressOf(this.readFramesData));
         this.nativeSource.setReadFramesCallback(Function.fromStaticFunction(PcmTransform.readFramesCallback));
@@ -181,96 +180,42 @@ private class PcmTransform {
 
     public final nativeSourceList: Star<NativeAudioSourceList>;
     public final transformFunction: PcmTransformFunction<T>;
-    public var transformData: Star<T>;
+    public final transformDataStar: Star<T>;
+    final transformData: T;
 
     public function new(
         nativeSourceList: Pointer<NativeAudioSourceList>,
-        transformFunction: PcmTransformFunction<T>
+        transformFunction: PcmTransformFunction<T>,
+        transformData: T
     ) {
         this.nativeSourceList = nativeSourceList.ptr;
         this.transformFunction = transformFunction;
+        this.transformData = transformData;
+        this.transformDataStar = cast Native.addressOf(this.transformData); // must be this.transformData
     }
 
 }
 
-/**
-    Experimental; not ready to use
-**/
-class GainNode extends PcmTransformNode<GainData> {
+class GainNode extends PcmTransformNode<Float> {
 
     public function new(context: AudioContext,  ?options: {
         var ?gain: Float;
     }) {
-        options = options == null ? {} : options;
-        var data = new GainData(context);
-        data.set(@:fixed {
-            gain: options.gain != null ? options.gain : 1.0,
-        });
-        super(context, Function.fromStaticFunction(applyGain), data);
+        var gainValue = options != null && options.gain != null ? options.gain : 1.0;
+        super(context, Function.fromStaticFunction(applyGain), gainValue);
     }
 
-    @:noDebug static function applyGain(gainData: Star<GainData>, nChannels: UInt32, frameCount: UInt32, interleavedPcmSamples: RawPointer<Float32>) {
-        // @! this is introducing too much hxcpp code onto the audio thread. Should not use this approach
-        var gain = gainData.get().gain;
-        Stdio.printf("applyGain(%f) %d %d %p\n", gain, nChannels, frameCount, interleavedPcmSamples);
-        // @! should use inline C++ for better auto-vectorization
-        for (i in 0...frameCount*nChannels) {
-            interleavedPcmSamples[i] *= gain;
-        }
+    @:noDebug static function applyGain(gainStar: Star<Float>, nChannels: UInt32, frameCount: UInt32, interleavedPcmSamples: RawPointer<Float32>) {
+        var gain: Float = Native.star(gainStar);
+        // we use inline C++ here because a for-loop will vectorize better than hxcpp's while-loop
+        untyped __cpp__('
+            int totalSamples = frameCount*nChannels;
+            for (int i = 0; i < totalSamples; i++) {
+                {0}[i] *= {1};
+            }
+        ', interleavedPcmSamples, gain);
     }
 
 }
-
-@:access(audio.AudioContext)
-@:generic private class LockedValue<T> {
-
-    final mutex: Star<Mutex>;
-
-    var value: T;
-    
-    public function new(context: AudioContext) {
-        this.mutex = Mutex.alloc();
-        this.mutex.init(context.maDevice.pContext);
-        cpp.vm.Gc.setFinalizer(this, Function.fromStaticFunction(LockedValueFinalizer.finalizer));
-    }
-
-    @:noDebug public inline function get(): T {
-        return mutex.locked(() -> value);
-    }
-
-    @:noDebug public inline function set(v: T): T {
-        return mutex.locked(() -> value = v);
-    }
-
-}
-
-@:access(audio.LockedValue)
-private class LockedValueFinalizer {
-    static public function finalizer<T>(instance: LockedValue<T>) {
-        #if debug
-        Stdio.printf("%s\n", "[debug] LockedValue.finalizer()");
-        #end
-        instance.mutex.uninit();
-        instance.mutex.free();
-    }
-}
-
-typedef GainData = LockedValue<{gain: Float}>;
-
-// private class GainData {
-
-//     public var gain: Float = 1.0; // @! needs mutex locked access
-//     public function new(gain = 1.0) {
-//         this.gain = gain;
-//         cpp.vm.Gc.setFinalizer(this, Function.fromStaticFunction(finalizer));
-//     }
-
-//     static function finalizer(instance: GainData) {
-//         #if debug
-//         Stdio.printf("%s\n", "[debug] GainData.finalizer()");
-//         #end
-//     }
-
-// }
 
 #end
