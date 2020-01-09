0f6a755447587175f39533bb7b7457603c17e7a6
George Corney <haxiomic@gmail.com>
2020-01-09 20:42:48 +0000

    Split out mixSources function

--- src/audio/AudioContext.hx
+++ src/audio/AudioContext.hx
@@ -136,7 +136,13 @@ class AudioContext {
     @:noDebug
     static function audioThread_deviceDataCallbackMixSources(maDevice: Star<Device>, output: Star<cpp.Void>, input: ConstStar<cpp.Void>, frameCount: UInt32) {
         var audioSourceList: Star<NativeAudioSourceList> = cast maDevice.pUserData;
-        untyped __global__.Audio_mixSources(audioSourceList, maDevice.playback.channels, frameCount, output);
+        var schedulingCurrentFrameBlock: UInt64 = 0;
+        mixSources(audioSourceList, maDevice.playback.channels, frameCount, schedulingCurrentFrameBlock, cast output);
+    }
+
+    @:noDebug
+    static inline function mixSources(sources: Star<NativeAudioSourceList>, nChannels: UInt32, frameCount: UInt32, schedulingCurrentFrameBlock: UInt64, output: Star<Float32>) {
+        return untyped __global__.Audio_mixSources(sources, nChannels, frameCount, schedulingCurrentFrameBlock, output);
     }
 
     static function finalizer(instance: AudioContext) {
