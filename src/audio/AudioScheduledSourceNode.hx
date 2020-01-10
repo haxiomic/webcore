884d5ac151e2c43cc5e8743af1642b9bcf4016a7
George Corney <haxiomic@gmail.com>
2020-01-10 19:19:48 +0000

    Support scheduling parameters when, offset and duration

--- src/audio/AudioScheduledSourceNode.hx
+++ src/audio/AudioScheduledSourceNode.hx
@@ -9,13 +9,34 @@ typedef AudioScheduledSourceNode = js.html.audio.AudioScheduledSourceNode;
 @:allow(audio.native.AudioContext)
 class AudioScheduledSourceNode extends AudioNode {
 
-    public function start(when: Float = 0.0) {
-        this.nativeNode.setActive(true);
-        this.nativeNode.setScheduledStartFrame(cast this.context.sampleRate * when);
+    /**
+        @throws String
+    **/
+    public function start(when: Float = 0.0, offset: Float = 0.0, ?duration: Float) {
+        if (nativeNode.getScheduledStartFrame() != -1) {
+            throw "Failed to execute 'start' on 'AudioBufferSourceNode': cannot call start more than once.";
+        }
+
+        nativeNode.setActive(true);
+        nativeNode.setScheduledStartFrame(cast context.sampleRate * when);
+
+        if (offset != 0.0 && decoder != null) {
+            decoder.seekToPcmFrame(cast decoder.sampleRate * offset);
+        }
+
+        if (duration != null) {
+            stop(when + duration);
+        }
     }
 
+    /**
+        @throws String
+    **/
     public function stop(when: Float = 0.0) {
-        this.nativeNode.setScheduledStopFrame(cast this.context.sampleRate * when);
+        if (nativeNode.getScheduledStartFrame() == -1) {
+            throw "Failed to execute 'stop' on 'AudioScheduledSourceNode': cannot call stop without calling start first";
+        }
+        nativeNode.setScheduledStopFrame(cast context.sampleRate * when);
     }
 
 }
