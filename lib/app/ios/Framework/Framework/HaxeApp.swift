458f18e22a17b84346ec807a98eb8ef639774465
George Corney <haxiomic@gmail.com>
2020-02-01 18:32:15 +0000

    Add swift files to wrapper framework

---
+++ lib/app/ios/Framework/Framework/HaxeApp.swift
@@ -0,0 +1,29 @@
+public class HaxeApp {
+    
+    static public func initialize() {
+        HaxeApp_initialize()
+    }
+
+    let ptr: UnsafeMutableRawPointer
+
+    public init() {
+        ptr = HaxeApp_create()
+    }
+
+    deinit {
+        HaxeApp_release(ptr)
+    }
+
+    public func onGraphicsContextReady() {
+        HaxeApp_onGraphicsContextReady(ptr)
+    }
+
+    public func onGraphicsContextLost() {
+        HaxeApp_onGraphicsContextLost(ptr)
+    }
+
+    public func onDrawFrame() {
+        HaxeApp_onDrawFrame(ptr)
+    }
+
+}
---
+++ lib/app/ios/Framework/Framework/HaxeAppViewController.swift
@@ -0,0 +1,75 @@
+import GLKit
+
+/**
+ * An OpenGL that hosts our MinimalGL example
+ */
+public class HaxeAppViewController: GLKViewController {
+    
+    public var haxeAppInstance: HaxeApp
+    public var context: EAGLContext?
+    
+    var haxeGraphicsContextReady = false
+    
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        HaxeApp.initialize()
+        haxeAppInstance = HaxeApp()
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        releaseGraphicsContext()
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        initializeGraphicsContext()
+    }
+    
+    private func initializeGraphicsContext() {
+        // Create an OpenGL ES context and store it in our local variable.
+        context = EAGLContext(api: .openGLES3)
+        
+        // Set the current EAGLContext to our context we created when performing OpenGL setup.
+        EAGLContext.setCurrent(context)
+        
+        // Perform checks and unwrap options in order to perform more OpenGL setup.
+        if let view = self.view as? GLKView, let context = context {
+            // Set our view's context to the EAGLContext we just created.s
+            view.context = context
+        }
+        
+        self.preferredFramesPerSecond = 60;
+    }
+    
+    
+    /// Perform cleanup, and delete buffers and memory.
+    private func releaseGraphicsContext() {
+        // Set the current EAGLContext to our context. This ensures we are deleting buffers against it and potentially not a
+        // different context.
+        EAGLContext.setCurrent(context)
+        
+        // Set the current EAGLContext to nil.
+        EAGLContext.setCurrent(nil)
+        
+        // Then nil out or variable that references our EAGLContext.
+        context = nil
+    }
+    
+    override public func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        // call onGraphicsContextReady() on first frame because at this point the view's drawable is fully initialized
+        // we also rely on the screen framebuffer being bound during initialization of the GLContext
+        if !haxeGraphicsContextReady {
+            haxeAppInstance.onGraphicsContextReady()
+            haxeGraphicsContextReady = true
+        }
+        
+        // let frameTime_s = CACurrentMediaTime()
+        
+        haxeAppInstance.onDrawFrame()
+    }
+
+}
