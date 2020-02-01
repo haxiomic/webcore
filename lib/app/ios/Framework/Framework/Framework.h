3dc5a85ffc87c36e608ce8aed39f819468136fbb
George Corney <haxiomic@gmail.com>
2020-02-01 15:17:31 +0000

    Add empty iOS Framework project (to wrap generated hxcpp lib)

---
+++ lib/app/ios/Framework/Framework/Framework.h
@@ -0,0 +1,19 @@
+//
+//  Framework.h
+//  Framework
+//
+//  Created by George Corney on 01/02/2020.
+//  Copyright © 2020 haxeapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework.
+FOUNDATION_EXPORT double FrameworkVersionNumber;
+
+//! Project version string for Framework.
+FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
+
+
