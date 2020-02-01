401e0940e339e4cca546fb78fe8dd3f5c82f899e
George Corney <haxiomic@gmail.com>
2020-02-01 19:56:35 +0000

    Include HaxeAppC in Framework wrapper

--- lib/app/ios/HaxeAppFramework/HaxeAppFramework.h
+++ lib/app/ios/HaxeAppFramework/HaxeAppFramework.h
@@ -16,4 +16,6 @@ FOUNDATION_EXPORT const unsigned char FrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Framework/PublicHeader.h>
 
-
+// This xcode project is copied into the hxcpp output folder, this file will be found in the include/ folder in that directory
+// The include/ directory has been added to the header search paths of this xcode project
+#include <HaxeAppC.h>
