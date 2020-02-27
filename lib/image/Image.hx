94243a314a7027b6213145faba94d93ec1424fa0
George Corney <haxiomic@gmail.com>
2020-02-27 19:41:15 +0000

    WIP image type (need to finish getData)

--- lib/image/Image.hx
+++ lib/image/Image.hx
@@ -6,11 +6,44 @@ package image;
 
 #if js
 
+import js.html.Blob;
+import js.html.URL;
+
 @:forward
 abstract Image(js.html.Image) from js.html.Image to js.html.Image {
 
-    static public function fromArrayBuffer(arrayBuffer: typedarray.ArrayBuffer) {
-        // return https://gist.github.com/candycode/f18ae1767b2b0aba568e
+    /**
+        **Asynchronously** decode an arraybuffer with the contents of a supported image file format
+        Supported formats depend on the browser
+    **/
+    static public function decodeImageData(arrayBuffer: typedarray.ArrayBuffer, ?successCallback: Image -> Void, ?errorCallback: String -> Void): Void {
+        // we don't provide an explicit mime type (like { type: 'image/jpeg' }), so we hope the browser is able to determine image type from the file header
+        var blob = new Blob([(arrayBuffer: js.lib.ArrayBuffer)]);
+        var objectUrl = URL.createObjectURL(blob);
+        var image = new js.html.Image();
+
+        function onError(e: Any) {
+            if (errorCallback != null) {
+                // usually there is no error message stored in the event
+                // using image.decode().catch() may allow accessing error information
+                errorCallback('Failed to decode image');
+            }
+            image.removeEventListener('error', onError);
+        }
+
+        function onLoad(e: Any) {
+            if (successCallback != null) {
+                successCallback(image);
+            }
+            image.removeEventListener('load', onLoad);
+        }
+
+        image.addEventListener('error', onError, true);
+        image.addEventListener('load', onLoad, true);
+
+        image.decoding = 'async';
+
+        image.src = objectUrl;
     }
 
 }
@@ -21,51 +54,101 @@ import cpp.*;
 import typedarray.ArrayBuffer;
 import image.native.StbImage;
 
+/**
+    Native Implementation of HTMLImageElement
+    Image data is stored internally with row packing alignment of 1 (i.e no padding bytes between rows)
+**/
 class Image {
 
-    // public var src(default, set): String;
+    public var width: Int;
+    public var height: Int;
     public var naturalWidth(default, null): Int = -1;
     public var naturalHeight(default, null): Int = -1;
+    public var src(default, set): String;
 
-    var nChannels: Int = -1;
-    var imageData: ArrayBuffer;
+    // internal image data
+    var nChannels (default, null): Int = -1;
+    var dataType (default, null): PixelDataType; 
+    var data (default, null): Null<ArrayBuffer> = null;
 
-    public function new() {
+    public function new(width: Int = 0, height: Int = 0) {
+        this.width = width;
+        this.height = height;
     }
     
-    // function set_src(v: String) {
-    //     // @! trigger load probably
-    //     return this.src = v;
-    // }
+    function getData(nChannels: Int, dataType: PixelDataType, flipY: Bool, forceUnpremultiply: Bool): Null<ArrayBuffer> {
+        // @! todo get original file arraybuffer
 
-    /**
-        @throws string If error during parsing
-    **/
-    static public function fromArrayBuffer(arrayBuffer: ArrayBuffer): Image {
-        // @! run on separate thread?
         var width: Int32 = -1;
         var height: Int32 = -1;
-        var nChannels: Int32 = -1;
+        var nChannelsInFile: Int32 = -1;
 
-        var imageBytes = StbImage.stbi_load_from_memory(arrayBuffer.toCPointer(), arrayBuffer.byteLength, Native.addressOf(width), Native.addressOf(height), Native.addressOf(nChannels), 0);
-
-        if (imageBytes == null || width == -1 || height == -1 || nChannels == -1) {
-            var failureReason = StbImage.stbi_failure_reason().toString();
-            throw 'Failed to parse image buffer: $failureReason';
+        var bytesPerChannel = switch dataType {
+            case UNSIGNED_BYTE: 1;
+            case FLOAT: 4;
         }
 
-        var image = new Image();
-        image.naturalWidth = width;
-        image.naturalHeight = height;
-        image.nChannels = nChannels;
+        StbImage.stbi_set_flip_vertically_on_load(flipY ? 1 : 0);
+        StbImage.stbi_set_unpremultiply_on_load(forceUnpremultiply ? 1 : 0);
 
-        // @! convert imageBytes
-        var byteLength = width * height * nChannels;
-        image.imageData = ArrayBuffer.fromCPointer(imageBytes, byteLength);
+        var imageBytes = switch dataType {
+            case UNSIGNED_BYTE: StbImage.stbi_load_from_memory(arrayBuffer.toCPointer(), arrayBuffer.byteLength, Native.addressOf(width), Native.addressOf(height), Native.addressOf(nChannelsInFile), nChannels);
+            case FLOAT: StbImage.stbi_loadf_from_memory(arrayBuffer.toCPointer(), arrayBuffer.byteLength, Native.addressOf(width), Native.addressOf(height), Native.addressOf(nChannelsInFile), nChannels);
+        }
+        
+        var byteLength = width * height * nChannels * bytesPerChannel;
 
-        return image;
+        return ArrayBuffer.fromCPointer(cast imageBytes, byteLength);
+    }
+    
+    function set_src(v: String) {
+        // @! trigger load probably
+        return this.src = v;
     }
 
+    /**
+        **Asynchronously** decode an arraybuffer with the contents of a supported image file format
+        See stb_image.h for supported image formats
+        Error handling managed by events (`onerror`)
+    **/
+    static public function decodeImageData(arrayBuffer: typedarray.ArrayBuffer, ?successCallback: Image -> Void, ?errorCallback: String -> Void): Void {
+        sys.thread.Thread.create(() -> {
+            var width: Int32 = -1;
+            var height: Int32 = -1;
+            var nChannels: Int32 = -1;
+            var bytesPerChannel = 1;
+
+            var imageBytes = StbImage.stbi_load_from_memory(arrayBuffer.toCPointer(), arrayBuffer.byteLength, Native.addressOf(width), Native.addressOf(height), Native.addressOf(nChannels), 0);
+
+            if (imageBytes == null || width == -1 || height == -1 || nChannels == -1) {
+                var failureReason = StbImage.stbi_failure_reason().toString();
+
+                if (errorCallback != null) {
+                    haxe.EntryPoint.runInMainThread(() -> errorCallback('Failed to parse image buffer: $failureReason'));
+                }
+            } else {
+                var image = new Image(width, height);
+                image.naturalWidth = width;
+                image.naturalHeight = height;
+                image.nChannels = nChannels;
+                image.dataType = UNSIGNED_BYTE;
+
+                var byteLength = width * height * nChannels * bytesPerChannel;
+                image.data = ArrayBuffer.fromCPointer(imageBytes, byteLength);
+
+                if (successCallback != null) {
+                    haxe.EntryPoint.runInMainThread(() -> successCallback(image));
+                }
+            }
+        });
+    }
+
+}
+
+// opengl-style image formats
+enum abstract PixelDataType(Int) {
+    var FLOAT;
+    var UNSIGNED_BYTE;
 }
 
 #end
