f1df141ea5048c252c78d3724a90cb6b2417a2f7
George Corney <haxiomic@gmail.com>
2020-03-11 13:17:38 +0000

    Docs

--- lib/app/HaxeAppInterface.hx
+++ lib/app/HaxeAppInterface.hx
@@ -7,15 +7,18 @@ import webgl.GLContext;
 	If the implementing class should not be the main app class then add `@:notMainApp`
 
 	**Units**
+
 	- `points` - Abstract length units independent of the display's physical pixel density. All coordinates and dimensions in this API are given in units of `points`. In UIKit this maps the `points` unit, in Android the `density independent pixel` and in HTML it maps to the `px` unit
 	- `pixels` - Corresponds to individually addressable values in a texture or display
 	
 	See [iOS Documentation: Points Verses Pixels](https://developer.apple.com/library/archive/documentation/2DDrawing/Conceptual/DrawingPrintingiOS/GraphicsDrawingOverview/GraphicsDrawingOverview.html#//apple_ref/doc/uid/TP40010156-CH14-SW7)
 
 	**Input**
+
+	Generally input events follow the latest browser input specs, however there are small differences, for example: to prevent the platform's default handling for an event, return `true` from an event handling method
 	- For mouse, touch and pen input, an interface that closely follows the PointerEvent API is used
-	- Wheel events mirror modern browser [WheelEvent](https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent) where all deltas are in units of **points**, normalizing for `deltaMode`
-	- KeyboardEvents mirror modern browser [KeyboardEvent](https://w3c.github.io/uievents/#idl-keyboardevent)
+	- Wheel events mirror browser [WheelEvent](https://developer.mozilla.org/en-US/docs/Web/API/WheelEvent) where all deltas are in units of **points**, normalizing for `deltaMode`
+	- KeyboardEvents mirror browser [KeyboardEvent](https://w3c.github.io/uievents/#idl-keyboardevent) with an extra parameter `hasFocus` to detect if the view is focused for the event
 **/
 @:nativeGen
 @:keep
