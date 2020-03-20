d7214935a2548fe396808b50f50e2c616b152008
George Corney <haxiomic@gmail.com>
2020-03-20 17:31:56 +0000

    Add DeviceInfo getSystemLanguageIsoCode()

---
+++ device/DeviceInfo.hx
@@ -0,0 +1,23 @@
+package device;
+
+class DeviceInfo {
+
+	/**
+		Returns a two letter [ISO 639-1 code](https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes) for the device's preferred language setting
+		For example: 'en' - English, 'de' - German
+	**/
+	static public function getSystemLanguageIsoCode(): String {
+		#if js
+		return js.Browser.navigator.language.substr(0, 2);
+		#else
+		#if (iphoneos || iphonesim || macos)
+
+		return device.native.CFLocale.preferredLanguagesFirst();
+
+		#else
+		return null;
+		#end
+		#end
+	}
+
+}
